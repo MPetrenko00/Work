276fb763c39ed5e81a4ea3f25939f7345f854d47
m.petrenko <mandarinkashusha@gmail.com>
2023-06-09 18:22:32 +0300

    if?-2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-"use srict";
-console.log('Hello world');
-document.getElementById('text').innerHTML = 'Текст для поля';
+/*"use srict";*/
+/*console.log('Hello world');
+document.getElementById('text').innerHTML = 'Текст для поля';*/
 
+/*
+//Синтаксис
+let message = "Hello!";
+
+if (2 > 1) {
+    //true
+    console.log(message);
+}*/
+
+
+let message = "Привіт";
+
+let first = 5;
+let second = 5;
+
+if(first === second){
+    console.log(message);
+}
 
